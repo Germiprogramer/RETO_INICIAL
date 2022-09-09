0e309e93d69ff97990d562b0a4acc174bf8339f9
rnoguer22 <rnogugon@myuax.com>
2022-09-09 16:50:11 +0200

    Metodo que nos proporciona el movimiento de los caballos de ajedrez

--- Reina.py
+++ Reina.py
@@ -1,19 +1,25 @@
-#Creamos la clase tablero que creara el tbalero nxn
 class Reina:
     def __init__(self, n):
         self.n = n
     
+    #Metodo que crea el tablero como una lista de listas
     def tablero(self):
         tablero = []
-        for i in range(n):
+        for i in range(self.n):
             fila = []
-            for m in range(n):
+            for m in range(self.n):
                 fila.append(" ")
             tablero.append(fila)
         return tablero
 
+    #Metodo para el posicionamiento de las reinas
+    #De esta manera elaboraremos un algoritmo que nos de una solucion para n
+    def mov_caballo(self):
+        #En esta funcion se define el posicionamiento como el movimiento de los caballos del ajedrez
+
 
 if __name__ == "__main__":
+
     n = int(input("Introduzca un numero: "))
     hola = Reina(n)
     print(hola.tablero())
