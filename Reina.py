6bf893e82496a9b0129b30c3b21db356b09c7c75
rnoguer22 <rnogugon@myuax.com>
2022-09-09 17:29:10 +0200

    Finalizacion del codigo

--- Reina.py
+++ Reina.py
@@ -13,21 +13,35 @@ class Reina:
         return tablero
 
 
+    #Metodo que comprueba que la solucion sea correcta
     def comprobacion(self):
+        solucion = False
+        #Aqui iria el codigo que verifica que la solucion sea correcta
+        if solucion:
+            return True
+        else:
+            return False
 
 
-
+    #Metodo que nos devuelve el tablero con una solucion
     def movimiento(self):
         for i in range(self.n):
             for j in range(self.n):
-                self.tablero[i][j] == "X"
-                self.comprobacion()            
+                self.tablero()[i][j] == "X"
+                if self.comprobacion():
+                    break
+                else:
+                    pass          
 
+
+    #Metodo que nos devuelve la solucion de las reinas
     def resultado(self):
+        self.movimiento()
         resultado = []
         for i in range(self.n):
             for j in range(self.n):
-                if self.tablero[i][j] == "X":
+                if self.tablero()[i][j] == "X":
+                    #Añadimos el indice de la lista donde esta la reina en la solucion final
                     resultado.append(self.tablero().index("X"))
                 else:
                     pass
@@ -38,5 +52,5 @@ class Reina:
 if __name__ == "__main__":
 
     n = int(input("Introduzca un numero: "))
-    hola = Reina(n)
-    print(hola.tablero())
+    solucion = Reina(n)
+    print(solucion.resultado())
